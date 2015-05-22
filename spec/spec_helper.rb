60d2d3b3d0e18190dcdb9e20e8a64c330ef79c6e
Koichi Shiraishi <zcheeee@gmail.com>
2015-05-23 06:01:27 +0900

    Add RubiMine specific RSpec settings
    Signed-off-by: Koichi Shiraishi <zcheeee@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,23 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-
 require 'simplecov'
-SimpleCov.start
-
 require 'coveralls'
-Coveralls.wear!
-
 require 'codeclimate-test-reporter'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+Coveralls.wear!
 CodeClimate::TestReporter.start
 
 require 'ruboty/docker'
+
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.order = 'random'
+  # config.formatter = 'documentation'
+  # config.formatter = 'Fuubar'
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
