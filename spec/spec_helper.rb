1fd3dac1121d575ea9812a5b4319b8fd93c647e3
Koichi Shiraishi <zcheeee@gmail.com>
2015-05-26 15:38:48 +0900

    Add CircleCI Artifacts
    Signed-off-by: Koichi Shiraishi <zcheeee@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,15 +8,17 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
 SimpleCov.start
 Coveralls.wear!
 CodeClimate::TestReporter.start
-
+if ENV['CIRCLE_ARTIFACTS']
+  require 'simplecov'
+  dir = File.join('..', '..', '..', ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
 require 'ruboty/docker'
 
 RSpec.configure do |config|
   config.color = true
   config.tty = true
   config.order = 'random'
-  # config.formatter = 'documentation'
-  # config.formatter = 'Fuubar'
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
