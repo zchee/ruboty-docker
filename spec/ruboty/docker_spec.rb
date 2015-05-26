266482052b2f59aecebdc34d026b0a4808cf251c
Koichi Shiraishi <zcheeee@gmail.com>
2015-05-26 15:41:12 +0900

    Add docker_host, docker_version, Fix Docker version spec Test to minor docker version Signed-off-by: Koichi Shiraishi <zcheeee@gmail.com>

--- spec/ruboty/docker_spec.rb
+++ spec/ruboty/docker_spec.rb
@@ -11,36 +11,30 @@ describe Ruboty::Handlers::Docker do
 
   let!(:docker) do
     Docker.url = ENV['RUBOTY_DOCKER_HOST']
-    Docker
+    @docker = Docker
   end
 
-  before(:all) {
-    ::Docker.url = ENV['RUBOTY_DOCKER_HOST']
-    @version     = ::Docker.version['Version']
-    @info        = ::Docker.info
-  }
-
   let(:docker_host) do
-    ENV['RUBOTY_DOCKER_HOST']
+    @docker.url
+  end
+
+  let(:require_docker_host) do
+    '1.6'
+  end
+
+  let(:docker_version) do
+    @docker.version['Version'][0..-3]
   end
 
   let(:docker_cerc_path) do
     ENV['DOCKER_CERT_PATH']
   end
 
-  let(:docker_version) do
-    Docker.version['Version']
-  end
-
   let!(:docker_info) do
-    Docker.info
+    docker.info
   end
 
   it 'must be "$RUBOTY_DOCKER_HOST"' do
-    expect(@version).to eq `/usr/local/bin/docker version | awk 'NR==6 {print$3}'`.gsub(/$\n/) { $1 }
-  end
-
-  it 'must be match Docker version' do
-    expect(@version).to eq '1.6.0'
+    expect(docker_version).to eq require_docker_host
   end
 end
